8b65fbeddb6bb65732915e5a396f7d86463ce72e
Zhongliu Li <zhongliuli@ZhongliudeMacBook-Pro.local>
2021-05-04 01:21:49 -0700

    first checkin

---
+++ src/navbar.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import './navbar.css';
+
+class NavBar extends Component {
+	render() {
+		return <div class="topnav">
+  					<a class="active" href="#home">Home</a>
+  					<a href="#news">News</a>
+  					<a href="#contact">Contact</a>
+  					<a href="#about">About</a>
+				</div>;
+	}
+}
+
+export default NavBar;
